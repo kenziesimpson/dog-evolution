75d1c8f24923e83ebb155245feaeb73d086e6f0c
unholydeath785 <ivansonofcoul@gmail.com>
2017-09-07 07:52:21 -0700

    Updated build system

--- index.js
+++ index.js
@@ -1,5 +1,74 @@
 const fs = require('fs')
-const JSON = require('JSON')
+const JSON = require('JSON');
+const generationDir = 'generations';
+const start = Date.now();
+
+// Creates generation directory if it does not exist
+function createGenerationsDir(next) {
+    fs.access(generationDir, fs.constants.F_OK, (err) => {
+        if (err && err.code === 'ENOENT') {
+            console.log(`[Build]: Creating directory at ${__dirname}/${generationDir}...`);
+            fs.mkdir(`./${generationDir}`, () => {
+                console.log(`[Build]: Created directory '${generationDir}'!`);
+                return next();
+            });
+        } else {
+            console.log(`[Build]: Directory at '${__dirname}/${generationDir}' already exists!`);
+            return next();
+        }
+    });
+}
+
+// Cleans the generationDir for the next run
+// @TODO
+function cleanGenerationDir(next) {
+    console.log(`[Build]: Cleaning 'generations' directory...`);
+    fs.readdir(`${__dirname}/${generationDir}/`, (err, files) => {
+        if (err) {
+            console.error(err);
+        }
+        let completed = 0;
+        files.forEach((file) => {
+            fs.unlink(`${__dirname}/${generationDir}/${file}`, (err) => {
+                if (err) {
+                    console.error(err);
+                }
+                if (++completed == files.length) {
+                    console.log(`[Build]: Cleaned 'generations' directory.`)
+                    return next()
+                }
+            });
+        })
+    })
+}
+
+// touches a temp file
+function createTempFile(next) {
+    console.log(`[Build]: Creating temp file...`);
+    fs.writeFile(`${__dirname}/${generationDir}/temp`, '', (err) => {
+        if (err) {
+            console.error(err);
+        }
+        console.log(`[Build]: Created a file named 'temp' in '${__dirname}/${generationDir}.`);
+        return next();
+    });
+}
+
+function executeSequentially(tasks, callback) {
+    function iterate(i) {
+        if (i == tasks.length) {
+            let end = Date.now();
+            console.log(`\n--- End Build (Time: ${end - start}ms) --- \n`);
+            callback();
+        } else {
+            tasks[i](() => {
+                iterate(i + 1);
+            });
+        }
+    }
+    
+    iterate(0);
+}
 
 /// FUNCTIONS ///
 
@@ -166,6 +235,8 @@ function containsDomesticated(generation, generationNum) {
 
 /// EXECUTED ///
 
+/// GLOBALS ///
+
 // Number of wolves
 let numWolves = 20
 let bestNum = numWolves / 4 + 1
@@ -178,126 +249,133 @@ let generation = generateFirst(numWolves)
 let metRequirement = false
 
 // Main loop
-while (!metRequirement) {
-  // If there is a domesticated dog, do not finsh this generation
-  metRequirement = containsDomesticated(generation, generationNum)
-  if (metRequirement) {
-    break;
-  }
-
-  // Calculate food values for a the current generation
-  for (let wolf = 0; wolf < Object.keys(generation).length; wolf++) {
-    let foodTotal = 0
-    if (isInPack(generation[wolf])) {
-      foodTotal += divideFoodPack(generation, numWolves)
-    }
-    foodTotal += divideFoodIndividual(generation[wolf])
-    foodTotal += divideFoodHumans(generation[wolf])
-    generation[wolf].food = foodTotal
-  }
-
-  // Calculate the best wolves of the generation
-  let bestWolves = {}
-  for (let wolf = 0; wolf < bestNum; wolf++) {
-    bestWolves[wolf] = generation[wolf]
-  }
-
-  for (let wolf = bestNum; wolf < numWolves; wolf++) {
-    // Find the lowest food value in bestWolves
-    let lowest = bestWolves[0]
-    for (let key in bestWolves) {
-      if (bestWolves[key] < lowest) {
-        lowest = bestWolves[key].food
-      }
-    }
-
-    // Replace the lowest food value in bestWolves
-    if (generation[wolf].food > lowest) {
-      // Find the key of the lowest value
-      for (let key in bestWolves) {
-        if (bestWolves[key].food == lowest) {
-          delete bestWolves[key]
+function main() {
+    while (!metRequirement) {
+        // If there is a domesticated dog, do not finsh this generation
+        metRequirement = containsDomesticated(generation, generationNum)
+        if (metRequirement) {
+          break;
+        }
+      
+        // Calculate food values for a the current generation
+        for (let wolf = 0; wolf < Object.keys(generation).length; wolf++) {
+          let foodTotal = 0
+          if (isInPack(generation[wolf])) {
+            foodTotal += divideFoodPack(generation, numWolves)
+          }
+          foodTotal += divideFoodIndividual(generation[wolf])
+          foodTotal += divideFoodHumans(generation[wolf])
+          generation[wolf].food = foodTotal
+        }
+      
+        // Calculate the best wolves of the generation
+        let bestWolves = {}
+        for (let wolf = 0; wolf < bestNum; wolf++) {
           bestWolves[wolf] = generation[wolf]
         }
+      
+        for (let wolf = bestNum; wolf < numWolves; wolf++) {
+          // Find the lowest food value in bestWolves
+          let lowest = bestWolves[0]
+          for (let key in bestWolves) {
+            if (bestWolves[key] < lowest) {
+              lowest = bestWolves[key].food
+            }
+          }
+      
+          // Replace the lowest food value in bestWolves
+          if (generation[wolf].food > lowest) {
+            // Find the key of the lowest value
+            for (let key in bestWolves) {
+              if (bestWolves[key].food == lowest) {
+                delete bestWolves[key]
+                bestWolves[wolf] = generation[wolf]
+              }
+            }
+          }
+        }
+      
+        // Evolution
+        let topPerformer = 0
+        for (let wolf = 1; wolf < bestNum; wolf++) {
+          if (bestWolves[wolf].food > bestWolves[topPerformer].food) {
+            topPerformer = wolf
+          }
+        }
+        let topWolf = bestWolves[topPerformer]
+        delete bestWolves[topPerformer]
+      
+        // Crossover
+        let newGeneration = {}
+        let currentWolf = 0
+        for (let i = 0; i < 2; i++) {
+          for (let wolf in bestWolves) {
+            newGeneration[currentWolf] = {
+              "pack": topWolf.pack,
+              "aggression": bestWolves[wolf].aggression,
+              "fear": topWolf.fear,
+              "plasticity": topWolf.plasticity,
+              "playfullness": bestWolves[wolf].playfullness,
+            }
+            currentWolf += 1
+      
+            newGeneration[currentWolf] = {
+              "pack": bestWolves[wolf].pack,
+              "aggression": topWolf.aggression,
+              "fear": bestWolves[wolf].fear,
+              "plasticity": bestWolves[wolf].plasticity,
+              "playfullness": topWolf.playfullness,
+            }
+            currentWolf += 1
+          }
+        }
+      
+        // Mutation
+        for (let wolf in newGeneration) {
+          // Account for selective breeding by humans
+          if (newGeneration[wolf].fear <= 0.3) {
+            newGeneration[wolf].plasticity = bound(newGeneration[wolf].plasticity + 0.02)
+          }
+          if (newGeneration[wolf].plasticity >= 0.35) {
+            newGeneration[wolf].playfullness = bound(newGeneration[wolf].playfullness + 0.02)
+            newGeneration[wolf].aggression = bound(newGeneration[wolf].aggression - 0.01)
+          }
+      
+          let toBeMutatedIndex = Math.trunc(50 * Math.random())
+          let toBeMutatedName
+          switch (toBeMutatedIndex) {
+            case 0:
+              toBeMutatedName = 'pack'
+              break;
+            case 1:
+              toBeMutatedName = 'aggression'
+              break;
+            case 2:
+              toBeMutatedName = 'fear'
+              break;
+            case 3:
+              toBeMutatedName = 'plasticity'
+              break;
+            case 4:
+              toBeMutatedName = 'playfullness'
+              break;
+            default:
+              toBeMutatedName = 'fear'
+              break;
+          }
+      
+          let newValue = bound(newGeneration[wolf][toBeMutatedName] + 0.15 * Math.random() - 0.1)
+          newGeneration[wolf][toBeMutatedName] = newValue
+        }
+        generationNum += 1
+      
+        writeGeneration(newGeneration, generationNum)
+        generation = newGeneration
       }
-    }
-  }
-
-  // Evolution
-  let topPerformer = 0
-  for (let wolf = 1; wolf < bestNum; wolf++) {
-    if (bestWolves[wolf].food > bestWolves[topPerformer].food) {
-      topPerformer = wolf
-    }
-  }
-  let topWolf = bestWolves[topPerformer]
-  delete bestWolves[topPerformer]
-
-  // Crossover
-  let newGeneration = {}
-  let currentWolf = 0
-  for (let i = 0; i < 2; i++) {
-    for (let wolf in bestWolves) {
-      newGeneration[currentWolf] = {
-        "pack": topWolf.pack,
-        "aggression": bestWolves[wolf].aggression,
-        "fear": topWolf.fear,
-        "plasticity": topWolf.plasticity,
-        "playfullness": bestWolves[wolf].playfullness,
-      }
-      currentWolf += 1
-
-      newGeneration[currentWolf] = {
-        "pack": bestWolves[wolf].pack,
-        "aggression": topWolf.aggression,
-        "fear": bestWolves[wolf].fear,
-        "plasticity": bestWolves[wolf].plasticity,
-        "playfullness": topWolf.playfullness,
-      }
-      currentWolf += 1
-    }
-  }
-
-  // Mutation
-  for (let wolf in newGeneration) {
-    // Account for selective breeding by humans
-    if (newGeneration[wolf].fear <= 0.3) {
-      newGeneration[wolf].plasticity = bound(newGeneration[wolf].plasticity + 0.02)
-    }
-    if (newGeneration[wolf].plasticity >= 0.35) {
-      newGeneration[wolf].playfullness = bound(newGeneration[wolf].playfullness + 0.02)
-      newGeneration[wolf].aggression = bound(newGeneration[wolf].aggression - 0.01)
-    }
-
-    let toBeMutatedIndex = Math.trunc(50 * Math.random())
-    let toBeMutatedName
-    switch (toBeMutatedIndex) {
-      case 0:
-        toBeMutatedName = 'pack'
-        break;
-      case 1:
-        toBeMutatedName = 'aggression'
-        break;
-      case 2:
-        toBeMutatedName = 'fear'
-        break;
-      case 3:
-        toBeMutatedName = 'plasticity'
-        break;
-      case 4:
-        toBeMutatedName = 'playfullness'
-        break;
-      default:
-        toBeMutatedName = 'fear'
-        break;
-    }
-
-    let newValue = bound(newGeneration[wolf][toBeMutatedName] + 0.15 * Math.random() - 0.1)
-    newGeneration[wolf][toBeMutatedName] = newValue
-  }
-
-  generationNum += 1
-
-  writeGeneration(newGeneration, generationNum)
-  generation = newGeneration
 }
+
+createGenerationsDir(() => {
+    const tasks = [createTempFile, cleanGenerationDir];
+    console.log('--- Start Build --- \n');
+    executeSequentially(tasks, main);
+});
